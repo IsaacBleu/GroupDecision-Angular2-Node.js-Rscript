6a8fe7ddfa37dd54c0a2756fc5184ebdab2e3d8d
Li-Wei Yang <isaac22.y@gmail.com>
2017-06-15 02:33:56 +0800

    Update app.module.ts

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -1,18 +1,26 @@
-﻿import { NgModule } from '@angular/core';
+﻿import { NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { NgModel } from '@angular/forms'
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { routing } from './app.routing';
 import { AppConfig } from './app.config';
+import { EqualValidator } from './register/equal-validator';
 
 import { AlertComponent } from './_directives/index';
 import { AuthGuard } from './_guards/index';
 import { AlertService, AuthenticationService, UserService } from './_services/index';
 import { HomeComponent } from './home/index';
 import { LoginComponent } from './login/index';
+import { CarouselComponent } from './login/index';
 import { RegisterComponent } from './register/index';
+import { SurveyAComponent } from './survey/index';
+import { SurveyBComponent } from './survey/index';
+import { ResultComponent } from './survey/index';
+import { Result2Component } from './survey/index';
+import { Ref1Component } from './reference/index';
 
 @NgModule({
     imports: [
@@ -26,14 +34,22 @@ import { RegisterComponent } from './register/index';
         AlertComponent,
         HomeComponent,
         LoginComponent,
-        RegisterComponent
+        CarouselComponent,
+        SurveyAComponent,
+        SurveyBComponent,
+        ResultComponent,
+        Result2Component,
+        RegisterComponent,
+        Ref1Component,
+        EqualValidator
     ],
     providers: [
         AppConfig,
         AuthGuard,
         AlertService,
         AuthenticationService,
-        UserService
+        UserService,
+        NgModel
     ],
     bootstrap: [AppComponent]
 })
