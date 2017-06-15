9de9e93a3b65e18a9ac35602913cc5ab6538d6b4
Li-Wei Yang <isaac22.y@gmail.com>
2017-06-15 15:22:54 +0800

    Create user.ts

--- client/app/_models/user.ts
+++ client/app/_models/user.ts
@@ -1,7 +1,30 @@
-﻿export class User {
+export class User {
     _id: string;
     username: string;
     password: string;
-    firstName: string;
-    lastName: string;
+    email: string;
+    a1c1g: number;
+    a1c1b: number;
+    a1c2g: number;
+    a1c2b: number;
+    a1c3g: number;
+    a1c3b: number;
+    c1_c2: number;
+    c1_c3: number;
+    c1_c4: number;
+    c1_c5: number;
+    c2_c3: number;
+    c2_c4: number;
+    c2_c5: number;
+    c3_c4: number;
+    c3_c5: number;
+    c4_c5: number;
+    user1: string;
+    user2: string;
+    user3: string;
+    out: string;
+    alt1: string;
+    alt2: string;
+    alt3: string;
+    alt4: string;
 }
